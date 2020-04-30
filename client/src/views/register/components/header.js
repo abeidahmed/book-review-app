4be78367bc01ba719c27381841ac40a86a78c831
abeidahmed <abeidahmed92@gmail.com>
2020-05-01 00:08:09 +0530

    Fix className

--- client/src/views/register/components/header.js
+++ client/src/views/register/components/header.js
@@ -9,11 +9,11 @@ const Header = () => {
       <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
         Sign up and get started
       </h2>
-      <p class="mt-2 text-center text-sm leading-5 text-gray-600">
+      <p className="mt-2 text-center text-sm leading-5 text-gray-600">
         Existing user?{" "}
         <Link
           to="/login"
-          class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+          className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
         >
           Sign in
         </Link>
