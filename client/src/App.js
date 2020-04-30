afcdf170a5e4c7ffb7d0a65d80326c0f61ca7bf1
abeidahmed <abeidahmed92@gmail.com>
2020-04-30 22:13:15 +0530

    Install cra and tailwindcss

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="text-indigo-500 bg-gray-100">hello world</header>
+    </div>
+  );
+}
+
+export default App;
