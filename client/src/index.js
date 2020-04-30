b238f1a8ad0402c137ff14b82735610a2f720e3a
abeidahmed <abeidahmed92@gmail.com>
2020-04-30 22:29:09 +0530

    Use absolute imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./assets/stylesheet/font.css";
-import "./assets/stylesheet/main.css";
+import "assets/stylesheet/font.css";
+import "assets/stylesheet/main.css";
 import App from "./App";
 
 ReactDOM.render(
