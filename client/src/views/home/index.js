ba600fda35147f3f24c616e1592207e39deaefc9
abeidahmed <abeidahmed92@gmail.com>
2020-05-02 00:12:06 +0530

    Pull dummy data from backend

--- client/src/views/home/index.js
+++ client/src/views/home/index.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
 export default function Home() {
+  const { data, loading, error } = useQuery(GET_BOOKS);
+
+  if (loading || error) return <p>Loading...</p>;
+
   return (
     <div>
-      <h1>hello from homepage</h1>
+      {data.books.map(book => (
+        <div key={book.title}>
+          <h1>{book.title}</h1>
+          <p>Author: {book.author}</p>
+          <p>{book.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
+
+const GET_BOOKS = gql`
+  query bookList {
+    books {
+      title
+      description
+      author
+    }
+  }
+`;
