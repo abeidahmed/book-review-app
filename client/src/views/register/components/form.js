d3b075bb5a1322fe7899f19ffc455d80d587c8bb
abeidahmed <abeidahmed92@gmail.com>
2020-05-02 00:12:25 +0530

    Make signup form functional

--- client/src/views/register/components/form.js
+++ client/src/views/register/components/form.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Form = () => {
+const Form = ({ signup }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    signup({
+      variables: {
+        email: email,
+        password: password
+      }
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="signup_email" className="text-sm text-gray-700 font-medium">
           Email address
         </label>
         <input
           id="signup_email"
-          name="email"
-          aria-label="Email address"
+          value={email}
           type="email"
+          onChange={e => setEmail(e.target.value)}
           className="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm placeholder-gray-500 text-gray-900 focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
         />
       </div>
@@ -21,14 +34,17 @@ const Form = () => {
         </label>
         <input
           id="signup_password"
-          name="password"
-          aria-label="Password"
+          value={password}
           type="password"
+          onChange={e => setPassword(e.target.value)}
           className="mt-1 block w-full px-3 py-2 rounded-md border border-gray-300 shadow-sm placeholder-gray-500 text-gray-900 focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
         />
       </div>
       <div className="mt-6">
-        <button className="block w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-7 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+        <button
+          type="submit"
+          className="block w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-7 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+        >
           Sign up
         </button>
       </div>
--- client/src/views/register/index.js
+++ client/src/views/register/index.js
@@ -1,16 +1,37 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { useMutation, useApolloClient } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 import Form from "./components/form";
 import Header from "./components/header";
 
 export default function Register() {
+  const client = useApolloClient();
+  const history = useHistory();
+  const [signup, { loading, error }] = useMutation(REGISTER_USER, {
+    onCompleted(signup) {
+      localStorage.setItem("token", signup.createUser.token);
+      client.writeData({ data: { isLoggedIn: true } });
+      history.push("/");
+    }
+  });
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full">
         <Header />
         <div className="mt-8 p-10 bg-white rounded-md shadow">
-          <Form />
+          <Form signup={signup} />
         </div>
       </div>
     </div>
   );
 }
+
+const REGISTER_USER = gql`
+  mutation signup($email: String!, $password: String!) {
+    createUser(userInput: { email: $email, password: $password }) {
+      token
+    }
+  }
+`;
