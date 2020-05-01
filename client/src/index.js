2015452a23b26c0cc80775adcb5666d225295630
abeidahmed <abeidahmed92@gmail.com>
2020-05-01 21:32:00 +0530

    Setup apollo-client

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
 import "assets/stylesheet/font.css";
 import "assets/stylesheet/main.css";
 import App from "App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://localhost:4000/"
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
