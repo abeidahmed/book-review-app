3199a6dfc6cdbaa0f8df443cdd1245e0755a0a95
abeidahmed <abeidahmed92@gmail.com>
2020-05-02 23:42:12 +0530

    Implement logout from admin navbar

--- client/src/admin/views/header/components/profile-link.js
+++ client/src/admin/views/header/components/profile-link.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { useApolloClient } from "@apollo/react-hooks";
+import { useHistory } from "react-router-dom";
 
 const ProfileLink = () => {
+  const client = useApolloClient();
+  const history = useHistory();
+
+  const handleLogout = () => {
+    client.writeData({ data: { isLoggedIn: false } });
+    localStorage.removeItem("token");
+    history.push("/");
+  };
+
   return (
     <div className="py-1 rounded-md bg-white shadow-xs">
       <a
@@ -15,7 +26,10 @@ const ProfileLink = () => {
       >
         Settings
       </a>
-      <button className="w-full text-left block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
+      <button
+        onClick={handleLogout}
+        className="w-full text-left block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
+      >
         Sign out
       </button>
     </div>
