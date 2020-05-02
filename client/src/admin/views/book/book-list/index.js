c39d9fa1bf3eeb0990371dfd128d9149a31cb908
abeidahmed <abeidahmed92@gmail.com>
2020-05-02 19:44:38 +0530

    Add book table

--- client/src/admin/views/book/book-list/index.js
+++ client/src/admin/views/book/book-list/index.js
@@ -1,8 +1,77 @@
 import React from "react";
 import { AdminLayout } from "components/layout";
+import Icon from "components/icon";
 
 const BookList = () => {
-  return <AdminLayout>hello world</AdminLayout>;
+  return (
+    <AdminLayout>
+      <div className="lg:flex lg:items-center lg:justify-between">
+        <div className="flex-1 min-w-0">
+          <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
+            Book list
+          </h2>
+        </div>
+        <div className="mt-5 flex lg:mt-0 lg:ml-4">
+          <span className="sm:ml-3 shadow-sm rounded-md">
+            <button
+              type="button"
+              className="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition duration-150 ease-in-out"
+            >
+              <Icon icon="plus" className="-ml-1 mr-2 h-5 w-5" />
+              Add book
+            </button>
+          </span>
+        </div>
+      </div>
+
+      <div className="mt-16 shadow overflow-hidden sm:rounded-lg">
+        <div className="align-middle inline-block w-full overflow-x-auto border-b border-gray-200">
+          <table className="w-full">
+            <thead>
+              <tr>
+                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  Title
+                </th>
+                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  Author
+                </th>
+                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  Category
+                </th>
+                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  Posted on
+                </th>
+                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+              </tr>
+            </thead>
+            <tbody className="bg-white">
+              <tr>
+                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                  <p className="text-sm leading-5 text-gray-900">Snow white and the seven dwarfs</p>
+                </td>
+                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                  Charles Dickens
+                </td>
+                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                  <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                    Active
+                  </span>
+                </td>
+                <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+                  25th July, 2016
+                </td>
+                <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+                  <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                    Edit
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </AdminLayout>
+  );
 };
 
 export default BookList;
