dd0f45dd48397353d39db0a0cf7d0df3b514d62d
abeidahmed <abeidahmed92@gmail.com>
2020-05-02 16:20:03 +0530

    Fix focus color

--- client/src/admin/views/sidebar/index.js
+++ client/src/admin/views/sidebar/index.js
@@ -8,7 +8,7 @@ const Sidebar = () => {
     <aside className="w-64 min-h-screen border-r bg-white flex-shrink-0">
       <div className="h-16 flex items-center justify-between px-3">
         <img className="h-8 w-auto" src={LogoFull} alt="logo" />
-        <button className="p-1 -mr-2 rounded-md text-gray-700 focus:outline-none focus:text-gray-900 focus:bg-gray-200 transition duration-150 ease-in-out">
+        <button className="lg:hidden p-1 -mr-2 rounded-md text-gray-700 focus:outline-none focus:text-gray-900 focus:bg-gray-100 transition duration-150 ease-in-out">
           <Icon icon="close" className="h-6 w-6" />
         </button>
       </div>
