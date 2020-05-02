fb62d6b2ee8fb8a65f9e7824acebc4781a2367d0
abeidahmed <abeidahmed92@gmail.com>
2020-05-02 15:33:21 +0530

    Create book lists page

--- client/src/admin/routes/admin-route.js
+++ client/src/admin/routes/admin-route.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import BookList from "admin/views/book/book-list";
 import Dashboard from "admin/views/dashboard";
 
 const AdminRoute = () => {
   return (
     <Switch>
-      <Route path="/admin" component={Dashboard} />
+      <Route exact path="/admin/books" component={BookList} />
+      <Route exact path="/admin" component={Dashboard} />
     </Switch>
   );
 };
---
+++ client/src/admin/views/book/book-list/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BookList = () => {
+  return (
+    <div>
+      <h1>booklist</h1>
+    </div>
+  );
+};
+
+export default BookList;
