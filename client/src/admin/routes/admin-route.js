7a841f26d785c57f3d180c3c5bc5ec0ee50975c2
abeidahmed <abeidahmed92@gmail.com>
2020-05-02 16:20:14 +0530

    Add header

--- client/src/admin/routes/admin-route.js
+++ client/src/admin/routes/admin-route.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import BookList from "admin/views/book/book-list";
 import Dashboard from "admin/views/dashboard";
+import Header from "admin/views/header";
 import Sidebar from "admin/views/sidebar";
 
 const AdminRoute = () => {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="w-full">
+      <div className="w-full flex flex-col">
+        <Header />
         <Switch>
           <Route path="/admin/books" component={BookList} />
           <Route path="/admin/dashboard" component={Dashboard} />
