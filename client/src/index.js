2784b8b534c9929fd32b96ec31bad6cca93755c6
abeidahmed <abeidahmed92@gmail.com>
2020-05-04 23:32:16 +0530

    Fix authentication bug

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloClient } from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
 import "assets/stylesheet/font.css";
 import "assets/stylesheet/main.css";
 import App from "App";
@@ -11,32 +12,33 @@ import { resolvers, typeDefs } from "graphql/resolvers";
 
 const cache = new InMemoryCache();
 
-const token = localStorage.getItem("token");
-let authorization;
-if (token) {
-  authorization = `Bearer ${token}`;
-} else {
-  authorization = "";
-}
-
-const link = new HttpLink({
-  headers: {
-    authorization
-  },
+const httpLink = new HttpLink({
   uri: "/graphql"
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache,
+  typeDefs,
+  resolvers
 });
 
 cache.writeData({
   data: {
-    isLoggedIn: !!token
+    isLoggedIn: !!localStorage.getItem("token")
   }
-});
-
-const client = new ApolloClient({
-  cache,
-  link,
-  typeDefs,
-  resolvers
 });
 
 ReactDOM.render(
