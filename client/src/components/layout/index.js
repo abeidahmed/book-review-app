821ad7ccf51fee982a2be6b7d23d1731218e1da7
abeidahmed <abeidahmed92@gmail.com>
2020-05-04 21:53:57 +0530

    Fix admin layout height

--- client/src/components/layout/index.js
+++ client/src/components/layout/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const AdminLayout = ({ children, ...props }) => (
-  <main style={{ height: "calc(100vh - 64px)" }} className="bg-gray-100 p-6 relative" {...props}>
+  <main style={{ minHeight: "calc(100vh - 64px)" }} className="bg-gray-100 p-6 relative" {...props}>
     {children}
   </main>
 );
