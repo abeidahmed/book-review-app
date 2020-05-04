2ea724f6ccdc731991015c1ec58838e84c5c998e
abeidahmed <abeidahmed92@gmail.com>
2020-05-04 21:15:35 +0530

    Add create category api

---
+++ client/src/api/category/create-category.js
@@ -0,0 +1,22 @@
+import gql from "graphql-tag";
+
+export const CREATE_CATEGORY = gql`
+  mutation createCategory($title: String!, $description: String) {
+    createCategory(categoryInput: { title: $title, description: $description }) {
+      _id
+      title
+      description
+      bookCount
+      createdAt
+      updatedAt
+      books {
+        _id
+        title
+        description
+        author
+        createdAt
+        updatedAt
+      }
+    }
+  }
+`;
