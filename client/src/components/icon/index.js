60c57c0a8f030a67b3cbb0065a7c0cc4b2e02df3
abeidahmed <abeidahmed92@gmail.com>
2020-05-06 00:37:47 +0530

    Add upload image icon

--- client/src/components/icon/index.js
+++ client/src/components/icon/index.js
@@ -2,6 +2,14 @@ import React from "react";
 
 const Glyph = ({ icon }) => {
   switch (icon) {
+    case "add-image":
+      return (
+        <g>
+          <path d="M4,5h13v7h2V5c0-1.103-0.897-2-2-2H4C2.897,3,2,3.897,2,5v12c0,1.103,0.897,2,2,2h8v-2H4V5z" />
+          <path d="M8 11L5 15 16 15 12 9 9 13z" />
+          <path d="M19 14L17 14 17 17 14 17 14 19 17 19 17 22 19 22 19 19 22 19 22 17 19 17z" />
+        </g>
+      );
     case "aeroplane":
       return (
         <g>
