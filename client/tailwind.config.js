ea3d57951ac2abd6c0cd15d7f78c74de221da894
abeidahmed <abeidahmed92@gmail.com>
2020-05-05 22:43:22 +0530

    Add error input class

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,8 +10,22 @@ module.exports = {
       zIndex: {
         "100": "100",
         ...defaultTheme.zIndex
+      },
+      boxShadow: {
+        errorOutline: "0 0 0 3px rgba(229, 62, 62, 0.5)"
       }
-    }
+    },
+    customForms: theme => ({
+      error: {
+        "input, textarea": {
+          borderColor: theme("colors.red.600"),
+          "&:focus": {
+            boxShadow: theme("boxShadow.errorOutline"),
+            borderColor: theme("colors.red.600")
+          }
+        }
+      }
+    })
   },
   variants: {},
   plugins: [require("@tailwindcss/ui")]
