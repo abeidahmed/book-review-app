cc739d21877e0eda5d0a611d4d3595c3929b93a9
abeidahmed <abeidahmed92@gmail.com>
2020-05-07 23:53:26 +0530

    Add exports

--- client/src/components/field/index.js
+++ client/src/components/field/index.js
@@ -1,3 +1,5 @@
 export { Checkbox } from "./checkbox";
+export { CustomInputField } from "./custom-input-field";
 export { InputField } from "./input-field";
+export { TextArea } from "./text-area";
 export { UploadFile } from "./upload-file";
